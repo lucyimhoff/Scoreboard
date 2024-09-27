86a3ab953087a887069b4f91ba2b87cd7da2cd1a
lucyimhoff <lucyi4@nycstudents.net>
2024-09-27 13:32:58 +0000

    recordPlay

--- Main.java
+++ Main.java
@@ -2,6 +2,20 @@ public class Main
 {
     public static void main(String[] args)
 {
+    String info;
+    Scoreboard game = new Scoreboard("Red", "Blue");
+    info = game.getScore();
+    System.out.println(info);
+    game.recordPlay(1);
+    info = game.getScore();
+    System.out.println(info);
+    game.recordPlay(0);
+    game.recordPlay(5);
+    game.recordPlay(0);
+    game.recordPlay(1);
+    game.recordPlay(0);
+    info = game.getScore();
+    System.out.println(info);
 
 }
 }
--- Scoreboard.java
+++ Scoreboard.java
@@ -1,11 +1,38 @@
 public class Scoreboard
 {
-private String teamone
-private String teamtwo
 
-public void recordPlay()
-{
-points = 0
+    private String teamOne;
+    private String teamTwo;
+    private int teamOneScore;
+    private int teamTwoScore;
+    private String activeTeam;
 
-}
+    public Scoreboard(String one, String two)
+    {
+        teamOne = one;
+        teamTwo = two;
+        activeTeam = teamOne;
+    }
+
+    public void recordPlay(int score)
+    {
+        if (score == 0)
+        {
+            if activeTeam = teamOne
+            {
+                activeTeam = teamTwo;
+            }
+
+        }
+        else
+        {
+            teamOneScore= teamOneScore + score; 
+        }
+    }
+
+    public String getScore()
+    {
+        return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
+
+    }
 }
